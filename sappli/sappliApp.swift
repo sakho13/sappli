db0394cc4e976b3331c2bbb08e05063e91af1dce
sakho13 <56495487+sakho13@users.noreply.github.com>
2023-09-05 12:48:34 +0900

    Initial Commit

---
+++ sappli/sappliApp.swift
@@ -0,0 +1,18 @@
+//
+//  sappliApp.swift
+//  sappli
+//
+//  Created on 2023/09/05.
+//  
+//
+
+import SwiftUI
+
+@main
+struct sappliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
